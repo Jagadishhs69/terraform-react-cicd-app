143bda3cbeb3b52c3c55bb9402b6a4c67e521692
Jagadishhs69 <hsjagadish42@gmail.com>
2025-08-18 19:31:08 +0530

    output "asg_name"       { value = "${var.env}-react-asg" } output "instance_sg_id" { value = "${var.env}-react-asg".id }

--- environment/dev/outputs.tf
+++ environment/dev/outputs.tf
@@ -1,4 +1,8 @@
 output "vpc_id"       { value = module.vpc.vpc_id }
 output "alb_dns_name" { value = module.alb.alb_dns_name }
-output "asg_name"     { value = module.asg.asg_name }
 output "ecr_repo"     { value = module.ecr.repository_url }
+output "asg_name" {
+  description = "Name of the Auto Scaling Group"
+  value       = module.asg.asg_name
+}
+
--- modules/asg/outputs.tf
+++ modules/asg/outputs.tf
@@ -1,2 +1,2 @@
-output "asg_name"       { value = aws_autoscaling_group.asg.name }
-output "instance_sg_id" { value = aws_security_group.ec2_sg.id }
+output "asg_name"       { value = "${var.env}-react-asg" }
+output "instance_sg_id" { value = "${var.env}-react-asg".id }
