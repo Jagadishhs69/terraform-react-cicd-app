99b5ae110be41efe5ee3ddc9a90aabf2461fe677
Jagadishhs69 <hsjagadish42@gmail.com>
2025-08-18 20:51:46 +0530

    value       = aws_vpc.this.id

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers { aws = { source = "hashicorp/aws", version = "~> 5.0" } }
 }
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "this" {
   cidr_block           = var.cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -10,13 +10,13 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.this.id
   tags   = { Name = "${var.env}-igw" }
 }
 
 resource "aws_subnet" "public" {
   for_each                = { for i, az in var.azs : i => az }
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = aws_vpc.this.id
   cidr_block              = cidrsubnet(var.cidr, 4, each.key)
   availability_zone       = each.value
   map_public_ip_on_launch = true
@@ -25,7 +25,7 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "private" {
   for_each          = { for i, az in var.azs : i => az }
-  vpc_id            = aws_vpc.main.id
+  vpc_id            = aws_vpc.this.id
   cidr_block        = cidrsubnet(var.cidr, 4, each.key + 4)
   availability_zone = each.value
   tags = { Name = "${var.env}-private-${each.value}" }
@@ -48,7 +48,7 @@ resource "aws_nat_gateway" "nat" {
 }
 
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.this.id
   route {
   cidr_block = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.igw.id
@@ -63,7 +63,7 @@ resource "aws_route_table_association" "public_assoc" {
 }
 
 resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.this.id
   route { 
     cidr_block = "0.0.0.0/0" 
     nat_gateway_id = aws_nat_gateway.nat.id 
