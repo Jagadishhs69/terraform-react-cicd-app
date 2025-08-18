51bc22b0a70b13eb66adac12e3a9b96c25ed58df
Jagadishhs69 <hsjagadish42@gmail.com>
2025-08-18 18:43:27 +0530

    block syntax fix

--- environment/dev/variables.tf
+++ environment/dev/variables.tf
@@ -1,16 +1,41 @@
 variable "env"                { type = string }
 variable "region"             { type = string }
 variable "vpc_cidr"           { type = string }
-variable "azs"                { type = list(string) }
+
 variable "ami_id"             { type = string }
 variable "instance_type"      { type = string }
 variable "desired_capacity"   { type = number }
 variable "min_size"           { type = number }
 variable "max_size"           { type = number }
-variable "ssh_cidr_blocks"    { type = list(string), default = [] }
+
 # CloudWatch
-variable "log_retention_days" { type = number, default = 14 }
-variable "cpu_high_threshold" { type = number, default = 80 }
-variable "alb_5xx_threshold"  { type = number, default = 5 }
-variable "create_sns"         { type = bool, default = false }
-variable "alert_email"        { type = string, default = "" }
+variable "log_retention_days" { 
+  type = number
+  default = 14 
+}
+variable "cpu_high_threshold" { 
+  type = number
+  default = 80 
+}
+variable "alb_5xx_threshold"  { 
+  type = number
+  default = 5 
+}
+variable "create_sns" { 
+  type = bool
+  default = false 
+}
+variable "alert_email" { 
+  type = string
+  default = "" 
+}
+
+variable "azs"{ 
+  type = list(string) 
+  default = []
+}
+
+variable "ssh_cidr_blocks"{ 
+  type = list(string)
+  default = [] 
+}
