a9aeb0485d9b0d5202205e835039ba3ae957e92e
Jagadishhs69 <hsjagadish42@gmail.com>
2025-08-19 15:01:11 +0530

    created private key to access instances using bastion host

--- modules/asg/outputs.tf
+++ modules/asg/outputs.tf
@@ -1,2 +1,4 @@
-output "asg_name"       { value = "${var.env}-react-asg" }
-output "instance_sg_id" { value = "${var.env}-react-asg.id" }
+output "asg_name"               { value = "${var.env}-react-asg" }
+output "instance_sg_id"         { value = "${var.env}-react-asg.id" }
+output "asg_key_name"           { value = aws_key_pair.asg_key.key_name}
+output "asg_private_key_path"   { value = local_file.asg_private_key.filename}
